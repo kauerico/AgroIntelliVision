cabf328902bec7eac47729ab6ca7ed8576ae2d8a
Katcilane silva de souza <143912823+KatSilvax@users.noreply.github.com>
2025-04-01 19:18:30 -0400

    feat: pasta config criada

---
+++ config/settings.py
@@ -0,0 +1,10 @@
+# Configurações globais
+DATASET_PATH = "/content/drive/MyDrive/seu_caminho_para_dataset"
+BATCH_SIZE = 64
+IMG_SIZE = (256, 256)
+EPOCHS = 40
+NUM_CLASSES = 10
+
+# Otimizações de performance
+AUTOTUNE = tf.data.AUTOTUNE
+MIXED_PRECISION = True
